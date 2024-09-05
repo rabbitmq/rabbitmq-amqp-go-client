e84325ff30040f9beba4fcaea189e6a04a5b3095
Gabriele Santomaggio <G.santomaggio@gmail.com>
2024-09-05 11:57:39 +0200

    remove unsed code
    Signed-off-by: Gabriele Santomaggio <G.santomaggio@gmail.com>

--- examples/getting_started/main.go
+++ examples/getting_started/main.go
@@ -3,24 +3,11 @@ package main
 import (
 	"context"
 	"fmt"
-	amqp "github.com/Azure/go-amqp"
 	"github.com/rabbitmq/rabbitmq-amqp-dotnet-client/rabbitmq_amqp"
 )
 
 func main() {
 
-	//nullGolangMessage := amqp.Message{}
-	//nullGolangMessage := amqp.NewMessage(nil)
-	nullGolangMessage := amqp.Message{}
-	var null byte
-	null = 64
-	nullGolangMessage.Value = null
-
-	binary, err1 := nullGolangMessage.MarshalBinary()
-	if err1 != nil {
-		return
-	}
-	fmt.Printf("Binary: %v\n", binary)
 	amqpConnection := rabbitmq_amqp.NewAmqpConnection()
 	err := amqpConnection.Open(context.Background(), rabbitmq_amqp.NewConnectionSettings())
 	if err != nil {
